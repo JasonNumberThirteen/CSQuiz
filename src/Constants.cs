be1d1015620b2ef73c1921931cbde107cab8a706
Jason <numberthirteen4026@gmail.com>
2022-08-26 13:59:44 +0200

    Added a constant for exception message

--- src/Constants.cs
+++ src/Constants.cs
@@ -5,6 +5,7 @@ namespace Quiz
 		public static readonly string GAME_TITLE = "QUIZ";
 		public static readonly string QUESTIONS_FILENAME = "questions.json";
 
+		public static readonly string INCORRECT_RANGE_OF_NUMBERS_EXCEPTION_MESSAGE = "Incorrect range of numbers! Minimum is greater than maximum!";
 		public static readonly string QUESTION_MESSAGE = "QUESTION";
 		public static readonly string CORRECT_ANSWER_MESSAGE = "Correct!";
 		public static readonly string WRONG_ANSWER_MESSAGE = "Wrong!";
--- src/Input.cs
+++ src/Input.cs
@@ -9,7 +9,7 @@ namespace Quiz
 
 			if(min > max)
 			{
-				throw new Exception("Incorrect range of numbers! Minimum is greater than maximum!");
+				throw new Exception(Constants.INCORRECT_RANGE_OF_NUMBERS_EXCEPTION_MESSAGE);
 			}
 
 			do
