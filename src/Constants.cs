4da6b1b971025eff1c08678fde694dfabaf7919b
Jason <numberthirteen4026@gmail.com>
2022-08-26 13:58:47 +0200

    Changed order of constants

--- src/Constants.cs
+++ src/Constants.cs
@@ -3,8 +3,9 @@ namespace Quiz
 	static class Constants
 	{
 		public static readonly string GAME_TITLE = "QUIZ";
-		public static readonly string QUESTION_MESSAGE = "QUESTION";
 		public static readonly string QUESTIONS_FILENAME = "questions.json";
+
+		public static readonly string QUESTION_MESSAGE = "QUESTION";
 		public static readonly string CORRECT_ANSWER_MESSAGE = "Correct!";
 		public static readonly string WRONG_ANSWER_MESSAGE = "Wrong!";
 		public static readonly string GAME_END_MESSAGE = "This is the end!";
