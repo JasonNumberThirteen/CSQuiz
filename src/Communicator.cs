3166df041589dd1f9c21445a987c592e42e149ce
Jason <numberthirteen4026@gmail.com>
2022-08-27 19:03:22 +0200

    Changed order of methods in Communicator class

--- src/Communicator.cs
+++ src/Communicator.cs
@@ -15,10 +15,10 @@ namespace Quiz
 		}
 
 		public void WriteRequestToTypeNumber(int min, int max) => Console.Write("{0} {1} {2} {3} {4}: ", Constants.TYPE_NUMBER_STRING, Constants.FROM_STRING, min, Constants.TO_STRING, max);
-		public void WriteEnd() => Console.WriteLine(Constants.GAME_END_MESSAGE);
-		public void WriteTotalPoints(int gained, int? max) => Console.WriteLine("{0}: {1}/{2}", Constants.POINTS_STRING.ToUpper(), gained, max);
 		public void WriteResult(bool answeredCorrectly) => Console.WriteLine(answeredCorrectly ? Constants.CORRECT_ANSWER_MESSAGE : Constants.WRONG_ANSWER_MESSAGE);
 		public void WriteGainedPoints(int gainedPoints, int points) => Console.WriteLine("{0} {1} {2} {3}!\n{0} {4} {5}!", Constants.YOU_HAVE_STRING, Constants.GAINED_STRING, gainedPoints, gainedPoints == 1 ? Constants.POINT_STRING : Constants.POINTS_STRING, points, points == 1 ? Constants.POINT_STRING : Constants.POINTS_STRING);
+		public void WriteEnd() => Console.WriteLine(Constants.GAME_END_MESSAGE);
+		public void WriteTotalPoints(int gained, int? max) => Console.WriteLine("{0}: {1}/{2}", Constants.POINTS_STRING.ToUpper(), gained, max);
 		private void WriteAnswer(int number, string answer) => Console.WriteLine("{0}) {1}", number, answer);
 	}
 }
