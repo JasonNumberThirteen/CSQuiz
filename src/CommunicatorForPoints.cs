86ca6def99cb86b61c63fd0f83d7e0818baa125b
Jason <numberthirteen4026@gmail.com>
2022-08-28 14:28:20 +0200

    Moved methods writing about points

--- src/CommunicatorForPoints.cs
+++ src/CommunicatorForPoints.cs
@@ -8,5 +8,8 @@ namespace Quiz
 		{
 			this.pointsCounter = pointsCounter;
 		}
+
+		public void WriteGainedPoints(int gainedPoints, int points) => Console.WriteLine("{0} {1} {2} {3}!\n{0} {4} {5}!", Constants.YOU_HAVE_STRING, Constants.GAINED_STRING, gainedPoints, gainedPoints == 1 ? Constants.POINT_STRING : Constants.POINTS_STRING, points, points == 1 ? Constants.POINT_STRING : Constants.POINTS_STRING);
+		public void WriteTotalPoints(int gained, int? max) => Console.WriteLine("{0}: {1}/{2}", Constants.POINTS_STRING.ToUpper(), gained, max);
 	}
 }
