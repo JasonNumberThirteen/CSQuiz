6efd122cb2f8c34074537484e1ceb7c9541ce5ed
Jason <numberthirteen4026@gmail.com>
2022-08-28 13:33:18 +0200

    Added QuestionDataWithPoints class

---
+++ src/QuestionDataWithPoints.cs
@@ -0,0 +1,17 @@
+namespace Quiz
+{
+	class QuestionDataWithPoints : QuestionData
+	{
+		public int Points {get; set;}
+
+		public override void Validate()
+		{
+			base.Validate();
+
+			if(Points < 0)
+			{
+				throw new Exception("Incorrect questions data! The amount of points is negative!");
+			}
+		}
+	}
+}
