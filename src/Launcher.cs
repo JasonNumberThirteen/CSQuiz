a6a4a58673ba3aefae21ccd7f2006110c4cd95d9
Jason <numberthirteen4026@gmail.com>
2022-08-29 20:01:19 +0200

    Added Launcher class

---
+++ src/Launcher.cs
@@ -0,0 +1,28 @@
+namespace Quiz
+{
+	class Launcher
+	{
+		public Launcher()
+		{
+			JSONQuestionsReader<QuestionDataWithPoints> questionsReader = new JSONQuestionsReader<QuestionDataWithPoints>(Constants.QUESTIONS_FILENAME);
+			PointsCounter pointsCounter = new PointsCounter(questionsReader);
+			PointsCommunicator communicator = new PointsCommunicator(pointsCounter);
+			Game<QuestionDataWithPoints> game = new Game<QuestionDataWithPoints>(questionsReader, communicator);
+
+			game.OnStart += communicator.WriteGameTitle;
+
+			pointsCounter.OnIncrease += communicator.WriteGainedPoints;
+
+			game.OnCorrectAnswer += communicator.WriteResult<QuestionDataWithPoints>;
+			game.OnCorrectAnswer += delegate(QuestionDataWithPoints qdwp, bool answeredCorrectly)
+			{
+				pointsCounter.Points += qdwp.Points;
+			};
+
+			game.OnEnd += communicator.WriteEnd;
+			game.OnEnd += communicator.WriteTotalPoints;
+
+			game.Start();
+		}
+	}
+}
--- src/Program.cs
+++ src/Program.cs
@@ -4,25 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			JSONQuestionsReader<QuestionDataWithPoints> questionsReader = new JSONQuestionsReader<QuestionDataWithPoints>(Constants.QUESTIONS_FILENAME);
-			PointsCounter pointsCounter = new PointsCounter(questionsReader);
-			PointsCommunicator communicator = new PointsCommunicator(pointsCounter);
-			Game<QuestionDataWithPoints> game = new Game<QuestionDataWithPoints>(questionsReader, communicator);
-
-			game.OnStart += communicator.WriteGameTitle;
-
-			pointsCounter.OnIncrease += communicator.WriteGainedPoints;
-
-			game.OnCorrectAnswer += communicator.WriteResult<QuestionDataWithPoints>;
-			game.OnCorrectAnswer += delegate(QuestionDataWithPoints qdwp, bool answeredCorrectly)
-			{
-				pointsCounter.Points += qdwp.Points;
-			};
-
-			game.OnEnd += communicator.WriteEnd;
-			game.OnEnd += communicator.WriteTotalPoints;
-
-			game.Start();
+			new Launcher();
 		}
 	}
 }
