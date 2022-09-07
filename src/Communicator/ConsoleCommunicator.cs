cda6e56dc9ea639c849af2918a39948f8b0c7c8a
Jason <numberthirteen4026@gmail.com>
2022-09-07 18:05:34 +0200

    Added Communicator class

--- src/Communicator/ConsoleCommunicator.cs
+++ src/Communicator/ConsoleCommunicator.cs
@@ -1,12 +1,12 @@
 namespace Quiz
 {
-	class ConsoleCommunicator
+	class ConsoleCommunicator : Communicator
 	{
-		public void WriteGameTitle() => Console.WriteLine(Constants.GAME_TITLE);
-		public void WriteQuestionHeader<T>(T t, int ordinalNumber) where T : QuestionData => Console.WriteLine("{0} {1}", Constants.QUESTION_MESSAGE, ordinalNumber);
-		public void WriteQuestion<T>(T t, int ordinalNumber) where T : QuestionData => Console.WriteLine(t.Question);
-
-		public void WriteAnswers<T>(T t, int ordinalNumber) where T : QuestionData
+		public override void WriteGameTitle() => Console.WriteLine(Constants.GAME_TITLE);
+		public override void WriteQuestionHeader<T>(T t, int ordinalNumber) => Console.WriteLine("{0} {1}", Constants.QUESTION_MESSAGE, ordinalNumber);
+		public override void WriteQuestion<T>(T t, int ordinalNumber) => Console.WriteLine(t.Question);
+		
+		public override void WriteAnswers<T>(T t, int ordinalNumber)
 		{
 			for (int a = 0; a < t.Answers!.Length; ++a)
 			{
@@ -14,11 +14,10 @@ namespace Quiz
 			}
 		}
 
-		public void WriteRequestToTypeNumber(int min, int max) => Console.Write("{0} {1} {2} {3} {4}: ", Constants.TYPE_NUMBER_STRING, Constants.FROM_STRING, min, Constants.TO_STRING, max);
-		public void WriteCorrectAnswer<T>(T t) where T : QuestionData => Console.WriteLine(Constants.CORRECT_ANSWER_MESSAGE);
-		public void WriteWrongAnswer<T>(T t) where T : QuestionData => Console.WriteLine(Constants.WRONG_ANSWER_MESSAGE);
-		public void WriteEnd() => Console.WriteLine(Constants.GAME_END_MESSAGE);
-		
-		private void WriteAnswer(int number, string answer) => Console.WriteLine("{0}) {1}", number, answer);
+		public override void WriteAnswer(int number, string answer) => Console.WriteLine("{0}) {1}", number, answer);
+		public override void WriteRequestToTypeNumber(int min, int max) => Console.Write("{0} {1} {2} {3} {4}: ", Constants.TYPE_NUMBER_STRING, Constants.FROM_STRING, min, Constants.TO_STRING, max);
+		public override void WriteCorrectAnswer<T>(T t) => Console.WriteLine(Constants.CORRECT_ANSWER_MESSAGE);
+		public override void WriteWrongAnswer<T>(T t) => Console.WriteLine(Constants.WRONG_ANSWER_MESSAGE);
+		public override void WriteEnd() => Console.WriteLine(Constants.GAME_END_MESSAGE);
 	}
 }
