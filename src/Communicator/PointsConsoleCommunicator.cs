cbfde9cdcb90066a37f2a91619f5a9e6fe667c99
Jason <numberthirteen4026@gmail.com>
2022-09-07 18:14:54 +0200

    Added PointsCommunicator class

--- src/Communicator/PointsConsoleCommunicator.cs
+++ src/Communicator/PointsConsoleCommunicator.cs
@@ -1,17 +1,14 @@
 namespace Quiz
 {
-	class PointsConsoleCommunicator<T> : ConsoleCommunicator where T : QuestionDataWithPoints
+	class PointsConsoleCommunicator<T> : PointsCommunicator<T> where T : QuestionDataWithPoints
 	{
-		private PointsCounter<T> pointsCounter;
-		
-		public PointsConsoleCommunicator(PointsCounter<T> pointsCounter)
+		public PointsConsoleCommunicator(PointsCounter<T> pointsCounter) : base(pointsCounter)
 		{
-			this.pointsCounter = pointsCounter;
+			
 		}
 
-		public void WriteGainedPoints(int gainedPoints, int points) => Console.WriteLine("{0} {1} {2} {3}!\n{0} {4} {5}!", PointsConstants.YOU_HAVE_STRING, PointsConstants.GAINED_STRING, gainedPoints, SingularOrPluralPointNoun(gainedPoints), points, SingularOrPluralPointNoun(points));
-		public void WriteTotalPoints() => Console.WriteLine("{0}: {1}/{2}", PointsConstants.POINTS_STRING.ToUpper(), pointsCounter.Points, pointsCounter.MaxPoints);
-
-		private string SingularOrPluralPointNoun(int value) => value == 1 ? PointsConstants.POINT_STRING : PointsConstants.POINTS_STRING;
+		public override void WriteGainedPoints(int gainedPoints, int points) => Console.WriteLine("{0} {1} {2} {3}!\n{0} {4} {5}!", PointsConstants.YOU_HAVE_STRING, PointsConstants.GAINED_STRING, gainedPoints, SingularOrPluralPointNoun(gainedPoints), points, SingularOrPluralPointNoun(points));
+		public override void WriteTotalPoints() => Console.WriteLine("{0}: {1}/{2}", PointsConstants.POINTS_STRING.ToUpper(), pointsCounter.Points, pointsCounter.MaxPoints);
+		public override string SingularOrPluralPointNoun(int value) => value == 1 ? PointsConstants.POINT_STRING : PointsConstants.POINTS_STRING;
 	}
 }
